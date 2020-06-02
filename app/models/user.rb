e91ab1eacc1d3f658358970eb6831bba8fb748a1
kavikalai <kavikalai110@gmail.com>
2020-06-02 10:40:43 +0530

    css styles changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
 	validates_format_of :contact_no, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
   attr_accessor :conform_password
 	validates_uniqueness_of :user_name, :email, :message=>"Validation faild"
-  #validates_presence_of :password, :with => /^[([a-z]|[A-Z])0-9_-]{6,40}$/, :message=> "must be at least 6 characters and include one number and one letter."
+  before_save :upcase_fields
+  validates_length_of :password, :minimum=> 6, :if=> Proc.new { |user| user.password.present? }
 
  
  
@@ -16,8 +17,13 @@ class User < ActiveRecord::Base
   named_scope :active, :conditions => ['status = ?', true]
   named_scope :inactive, :conditions => ['status =?',false]
   
+  
   def send_mail_notification
     UserMailer.deliver_sending_mail(self)
+  end
+
+  def upcase_fields
+   self.user_name = self.user_name.split.collect(&:capitalize).join(' ') if self.user_name && !self.user_name.blank?
   end
 
   def self.create_user(params)
